79c7369d88108838049f441742627a45c751f2d9
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-04-19 18:51:39 +0300

    tab and form references

---
+++ scripts/authentication.js
@@ -0,0 +1,6 @@
+// Handles authentication logic for index.html
+
+const loginTab = document.getElementById('login-tab');
+const registerTab = document.getElementById('register-tab');
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
