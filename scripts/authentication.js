21d9f202db0fcbb91e1641279938517294b37fe6
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-04-19 18:57:54 +0300

    login / registor form

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -4,3 +4,19 @@ const loginTab = document.getElementById('login-tab');
 const registerTab = document.getElementById('register-tab');
 const loginForm = document.getElementById('login-form');
 const registerForm = document.getElementById('register-form');
+
+// login / registor form 
+loginTab.addEventListener('click', () => {
+    loginTab.classList.add('active');
+    registerTab.classList.remove('active');
+    loginForm.classList.add('active');
+    registerForm.classList.remove('active');
+  });
+  
+  registerTab.addEventListener('click', () => {
+    registerTab.classList.add('active');
+    loginTab.classList.remove('active');
+    registerForm.classList.add('active');
+    loginForm.classList.remove('active');
+  });
+  
