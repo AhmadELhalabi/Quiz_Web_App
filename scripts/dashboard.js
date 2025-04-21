2f336c4a054ba1fc73b626a80cfbdc7c416e8f8f
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-04-21 15:00:00 +0300

    table to display data

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -14,5 +14,12 @@ window.addEventListener('DOMContentLoaded', () => {
         const scoreText = userScores.length
           ? userScores.map(s => `${s.quizTitle}: ${s.score}`).join(', ')
           : 'No scores yet';
+
+          const row = document.createElement('tr');
+          row.innerHTML = `
+          <td>${user.email}</td>
+          <td colspan="2">${scoreText}</td>
+      `;
+      tableBody.appendChild(row);
     })
 })
